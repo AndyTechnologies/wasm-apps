c7ab52b8032f58c38be002c4c44f256de27a9230
AndyTechnologies <garciandy579@gmail.com>
2026-07-17 21:57:08 -0400

    chore(examples): actualiza ejemplos wasm

--- examples/proyecto-completo/src/main.wasm.ts
+++ examples/proyecto-completo/src/main.wasm.ts
@@ -1,20 +1,20 @@
-import { add, multiply, fibonacci } from "./math.wasm";
+import { add, multiply, fibonacci } from './math.wasm';
 
 function printMenu(): void {
-  console.log("=== Calculadora WASM ===");
-  console.log("Operaciones disponibles:");
-  console.log("  suma(5, 3)      = " + add(5, 3).toString());
-  console.log("  multiplica(4, 6) = " + multiply(4, 6).toString());
+  console.log('=== Calculadora WASM ===');
+  console.log('Operaciones disponibles:');
+  console.log('  suma(5, 3)      = ' + add(5, 3).toString());
+  console.log('  multiplica(4, 6) = ' + multiply(4, 6).toString());
 }
 
 function printFibonacci(n: number): void {
-  console.log("Fibonacci(" + n.toString() + ") = " + fibonacci(n).toString());
+  console.log('Fibonacci(' + n.toString() + ') = ' + fibonacci(n).toString());
 }
 
 export function _start(): void {
   printMenu();
-  console.log("");
-  console.log("Serie de Fibonacci:");
+  console.log('');
+  console.log('Serie de Fibonacci:');
   for (let i = 0; i <= 10; i++) {
     printFibonacci(i);
   }
